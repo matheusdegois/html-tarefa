093a5276bd4a66afbc13c0e5a41bc2c588b09f02
matheusdegois <163362900+matheusdegois@users.noreply.github.com>
2024-09-03 22:24:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+ var nome = 'Matheus'
+ var idade =  19
+ var cidade = 'Laranjeiras do Sul'
+ 
+ console.log(
+     nome, idade, cidade
+ );
+
+ var nomeCompleto = 'matheus de gois'
+ var minhaIdade = 19
+ var estudante = true
+
+ typeof nomeCompleto;
+ typeof minhaIdade;
+ typeof estudante;
+
+ console.log(
+    nomeCompleto, minhaIdade, estudante
+ );
+
+ var inteiro = 8
+ var inteiro2 = 2
+
+ let soma=inteiro+inteiro2 
+ let sub=inteiro-inteiro2 
+ let produto=inteiro*inteiro2 
+ let divisao=inteiro/inteiro2 
+ let resto=inteiro%inteiro2 
+
+ console.log(
+    soma, sub, produto, divisao, resto
+ );
+
+
+ 
