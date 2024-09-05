4f2e9ea2da521cb6c2cea50ce4a993c1a772162f
matheusdegois <163362900+matheusdegois@users.noreply.github.com>
2024-09-05 19:59:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
  var cidade = 'Laranjeiras do Sul'
  
  console.log(
-     nome, idade, cidade
+     nome + idade + cidade
  );
 
  var nomeCompleto = 'matheus de gois'
@@ -15,21 +15,84 @@
  typeof estudante;
 
  console.log(
-    nomeCompleto, minhaIdade, estudante
+    nomeCompleto + minhaIdade + estudante
  );
 
  var inteiro = 8
  var inteiro2 = 2
 
- let soma=inteiro+inteiro2 
- let sub=inteiro-inteiro2 
- let produto=inteiro*inteiro2 
- let divisao=inteiro/inteiro2 
- let resto=inteiro%inteiro2 
+ let soma = inteiro + inteiro2 
+ let sub = inteiro - inteiro2 
+ let produto = inteiro * inteiro2 
+ let divisao = inteiro / inteiro2 
+ let resto = inteiro % inteiro2 
 
  console.log(
-    soma, sub, produto, divisao, resto
+   'A soma deles é:', soma +'A subtração deles é:', sub +'A multiplicação deles é:', produto +'A divisão deles é', divisao +'O resto deles é', resto
  );
+
+var valor = 10
+
+valor += 4;
+
+console.log(
+   'O valor após o operador +=', valor
+);
+
+valor -= 4;
+
+console.log(
+   'O valor após o operador -=', valor
+);
+
+valor *= 4;
+
+console.log(
+   'O valor após o operador *=', valor
+);
+
+valor /= 4;
+
+console.log(
+   'O valor após o operador /=', valor
+);
+ 
+var a = true
+var b = false
+var c = true
+
+let and = (a && c);
+
+let or = (a && b);
+
+let not = !a
+
+console.log(
+   'o valor de a and c será:', and + 'o valor de a or b será:', or + 'o valor de not a será:', not
+);
+
+var idade1 = 19
+
+if (idade1 >= 18){
+console.log('Você é maior de idade');
+}else{
+console.log('Você é menor de idade');
+}
+
+var nota = 4.6
+
+if ((nota > 7)){
+   console.log('Aprovado')
+}else if ((nota > 5 , and , nota<6.9)){
+   console.log('Recuperação')
+}else if ((nota < 5)){
+   console.log('Reprovado')
+}else {console.log('Você digitou um número não valído')}
+
+
+
+
+
 
 
  
